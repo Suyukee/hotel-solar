a7dcfbe2dfbddbb04d20dd6cb5a00186ff84d525
Suyukee <diefuck@ya.ru>
2024-05-23 22:32:43 +0300

    Fixed sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -2,32 +2,41 @@ import Link from 'next/link';
 
 function Sidebar({ toggleSidebar }) {
 	return (
-		<button className="menu-btn__button" onClick={toggleSidebar}>
-			<aside className="sidebar">
-				<div className="sidebar__menu-btn">
+		<aside className="sidebar">
+			<div className="sidebar__menu-btn">
+				<button className="menu-btn__button" onClick={toggleSidebar}>
 					<svg width="40px" height="40px" viewBox="0 0 24 24">
 						<path
 							d="M6.99486 7.00636C6.60433 7.39689 6.60433 8.03005 6.99486 8.42058L10.58 12.0057L6.99486 15.5909C6.60433 15.9814 6.60433 16.6146 6.99486 17.0051C7.38538 17.3956 8.01855 17.3956 8.40907 17.0051L11.9942 13.4199L15.5794 17.0051C15.9699 17.3956 16.6031 17.3956 16.9936 17.0051C17.3841 16.6146 17.3841 15.9814 16.9936 15.5909L13.4084 12.0057L16.9936 8.42059C17.3841 8.03007 17.3841 7.3969 16.9936 7.00638C16.603 6.61585 15.9699 6.61585 15.5794 7.00638L11.9942 10.5915L8.40907 7.00636C8.01855 6.61584 7.38538 6.61584 6.99486 7.00636Z"
 							fill="#ffffff"
 						></path>
 					</svg>
-				</div>
-				<div className="sidebar__menu">
-					<Link className="nav__link" href="#rooms">
+				</button>
+			</div>
+			<div className="sidebar__menu">
+				<Link className="menu__link" href="#rooms">
+					<button className="link__button" onClick={toggleSidebar}>
 						Номера
-					</Link>
-					<Link className="nav__link" href="/">
+					</button>
+				</Link>
+
+				<Link className="nav__link" href="#location">
+					<button className="link__button" onClick={toggleSidebar}>
 						Расположение
-					</Link>
-					<Link className="nav__link" href="/">
+					</button>
+				</Link>
+				<Link className="nav__link" href="#comforts">
+					<button className="link__button" onClick={toggleSidebar}>
 						Удобства
-					</Link>
-					<Link className="nav__link" href="/">
+					</button>
+				</Link>
+				<Link className="nav__link" href="#contacts">
+					<button className="link__button" onClick={toggleSidebar}>
 						Контакты
-					</Link>
-				</div>
-			</aside>
-		</button>
+					</button>
+				</Link>
+			</div>
+		</aside>
 	);
 }
 
